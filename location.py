af1ad147b220918c1db66b6ed478f54ea595418e
Denaos <bkeokot@calpoly.edu>
2019-04-05 10:51:22 -0700

    final version

--- location.py
+++ location.py
@@ -1,6 +1,8 @@
 # CPE 202 Lab 0
 
 # represents a location using name, latitude and longitude
+
+import math
 class Location:
     def __init__(self, name, lat, lon):
         self.name = name    # string for name of location
@@ -8,7 +10,7 @@ class Location:
         self.lon = lon      # longitude in degrees (-180 to 180)
 
     def __eq__(self, other):
-        return self.lat == other.lat and self.lon == other.lon
+        return type(other) == Location and math.isclose(self.lat,other.lat) and math.isclose(self.lon,other.lon)
 
     def __repr__(self):
         return "Location('%s', %g, %g)" % (self.name, self.lat, self.lon)
--- location_tests.py
+++ location_tests.py
@@ -5,35 +5,39 @@ class TestLab1(unittest.TestCase):
 
     def test_repr(self):
         "runs multiple test cases checking the repr function"
-        loc1 = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
-        loc2 = Location("Cal Poly", 32.5, -500)
-        self.assertEqual(repr(loc2),"Location('Cal Poly', 32.5, -500)")
-        loc3 = Location("Cal Poly", 32.5, -500)
-        self.assertNotEqual(repr(loc3),"Location('SLO', 35.3, -120.7)")
-        loc4 = Location("Poly Canyon Village", -500000, 0.5)
-        self.assertNotEqual(repr(loc4),"Location('SLO', 35.3, -120.7)")
-        loc5 = Location("Poly Canyon Village", -500000, 0.5)
-        self.assertEqual(repr(loc5),"Location('Poly Canyon Village', -500000, 0.5)")
+        loc = Location("SLO", 35.3, -120.7)
+        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        loc = Location("Cal Poly", 32.5, -500)
+        self.assertEqual(repr(loc),"Location('Cal Poly', 32.5, -500)")
+        loc = Location("Cal Poly", 32.5, -500)
+        self.assertNotEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        loc = Location("Poly Canyon Village", -500000, 0.5)
+        self.assertNotEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        loc = Location("Poly Canyon Village", -500000, 0.5)
+        self.assertEqual(repr(loc),"Location('Poly Canyon Village', -500000, 0.5)")
 
-    def test_eq(self):
+    def test_eq_true_false(self):
         "runs multiple test cases checking the equal sign"
-        loc6 = Location("SLO", 35.3, -120.7)
-        loc7 = Location("SLO", 35.3, -120.7)
-        self.assertEqual(loc6 == loc7, True)
-        loc8 = Location("SLO", 35.3, -120.7)
-        loc9 = Location("Cal Poly", 32.5, -500)
-        self.assertEqual(loc8 == loc9, False)
-        loc10 = Location("Cal Poly", 32.5, -500)
-        loc11 = Location("Cal Poly", 32.5, -500)
-        self.assertEqual(loc10 == loc11, True)
-        loc12 = Location("Poly Canyon Village", -500000, 0.5)
-        loc13 = Location("Poly Canyon Village", -500000, 0.5)
-        self.assertEqual(loc12 == loc13, True)
-        loc14 = Location("SLO", 35.3, -120.7)
-        loc15 = Location("Poly Canyon Village", -500000, 0.5)
-        self.assertEqual(loc14 == loc15, False)
+        loc = Location("SLO", 35, -120)
+        loc2 = Location("SLO", 35, -120)
+        self.assertEqual(loc == loc2, True)
+        loc = Location("SLO", 35, -120)
+        loc2 = Location("Cal Poly", 32, -500)
+        self.assertEqual(loc == loc2, False)
 
+    def test_eq_location(self):
+        "tests the eq function for the same type"
+        loc10 = Location("Cal Poly", 32.5, -500)
+        loc11 = 400
+        self.assertEqual(loc10 == loc11, False)
+
+    def test_eq_float(self):
+        loc12 = Location("Poly Canyon Village", -500.007, 0.5)
+        loc13 = Location("Poly Canyon Village", -500.007, 0.5)
+        self.assertAlmostEqual(loc12 == loc13, True)
+        loc14 = Location("SLO", 35.3, -120.7)
+        loc15 = Location("Poly Canyon Village", -500.007, 0.5)
+        self.assertAlmostEqual(loc14 == loc15, False
 
 if __name__ == "__main__":
         unittest.main()

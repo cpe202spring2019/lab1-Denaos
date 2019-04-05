29a2c4ef2b0f25fb5c089d1209ebdb738dc8f464
Denaos <bkeokot@calpoly.edu>
2019-04-05 10:52:43 -0700

    version 1.01

--- lab1.py
+++ lab1.py
@@ -17,9 +17,14 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
     """recursively reverses a list of numbers and returns the reversed list
     If list is None, raises ValueError"""
+    if int_list is None:
+        raise ValueError
+
     pass
 
 def bin_search(target, low, high, int_list):  # must use recursion
     """searches for target in int_list[low..high] and returns index if found
     If target is not found returns None. If list is None, raises ValueError """
+    if int_list is None:
+        raise ValueError
     pass
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -4,21 +4,43 @@ from lab1 import *
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
-    def test_max_list_iter(self):
-        """add description here"""
+    def test_max_list_iter_error(self):
+        """Tests the max_list_iter function for the value error"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+
+    def test_max_list_iter_empty(self):
+        """"Tests the max_list_iter function if the list is empty"""
         tlist = []
         self.assertEqual(max_list_iter(tlist), None)
-        tlist = [10,11,500,25]
-        self.assertEqual(max_list_iter(tlist),500)
-        tlist = [-22,-50,2,49,49.1]
-        self.assertEqual(max_list_iter(tlist),49.1)
-        tlist = [-22,-50,-4900,-49.1]
-        self.assertEqual(max_list_iter(tlist),-22)
-        tlist = [-0.999, -1, -4, -0.075, 0, -0.001]
-        self.assertEqual(max_list_iter(tlist),0)
+
+    def test_max_list_iter_order(self):
+        """"Tests the max_list_iter function of the order of the numbers"""
+        tlist = [500, 10, 11, 25]
+        self.assertEqual(max_list_iter(tlist), 500)
+        tlist = [10, 500, 11, 25]
+        self.assertEqual(max_list_iter(tlist), 500)
+        tlist = [10, 11, 500, 25]
+        self.assertEqual(max_list_iter(tlist), 500)
+        tlist = [10, 11, 25, 500]
+        self.assertEqual(max_list_iter(tlist), 500)
+
+    def test_max_list_iter_neg(self):
+        """""Tests the max_list_iter function with negative values"""
+        tlist = [-100, -50, -25, -10]
+        self.assertEqual(max_list_iter(tlist), -10)
+        tlist = [-5000, -7500, -2500, -1000]
+        self.assertEqual(max_list_iter(tlist), -1000)
+        tlist = [-50, -49, -51, -10, -75, -99]
+        self.assertEqual(max_list_iter(tlist), -10)
+
+    def test_max_list_iter_float(self):
+        """"Tests the max_list_iter function with floating values"""
+        tlist = [1.01, 1.25, 1.005, 1.34, 1.02]
+        self.assertEqual(max_list_iter(tlist), 1.34)
+        tlist = [25.25, 10.10, 35.35, 9.09]
+        self.assertEqual(max_list_iter(tlist), 35.35)
 
     def test_reverse_rec(self):
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
